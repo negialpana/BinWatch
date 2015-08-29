9634a7445e6127e0a6ec5bda1adfa5e01e0c7fc5
Alpana Negi <anegi@20EX20111-MAC.local>
2015-08-29 14:56:40 +0530

    #SmartBin project creation

---
+++ SmartBin/SmartBin/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SmartBin
+//
+//  Created by Alpana Negi on 8/29/15.
+//  Copyright © 2015 Alpana Negi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ SmartBin/SmartBin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartBin
+//
+//  Created by Alpana Negi on 8/29/15.
+//  Copyright © 2015 Alpana Negi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
