57a9f677a090deb9e69318a44fedf6374d052431
Ponnie Rohith <ponnierohith+github@gmail.com>
2015-10-07 21:13:28 +0530

    removeObserver

--- BinWatch/BinWatch/ViewControllers/BWFillLevelsViewController.m
+++ BinWatch/BinWatch/ViewControllers/BWFillLevelsViewController.m
@@ -71,6 +71,9 @@ NSMutableArray *activeBins;
 
 }
 
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
