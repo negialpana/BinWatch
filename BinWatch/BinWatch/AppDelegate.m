9a215d5846d22b5d4ec7fac7c1c6057775fbb2ca
Seema Kadavan <seema.kadavan@gmail.com>
2015-10-09 18:44:47 +0530

    Parse Integrated - Push notification being received from Parse Dashboard

--- BinWatch/BinWatch/AppDelegate.m
+++ BinWatch/BinWatch/AppDelegate.m
@@ -13,6 +13,7 @@
 #import <GoogleMaps/GoogleMaps.h>
 #import <Fabric/Fabric.h>
 #import <Crashlytics/Crashlytics.h>
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -26,7 +27,21 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
     [Fabric with:@[[Crashlytics class]]];
+
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [application registerUserNotificationSettings:settings];
+    [application registerForRemoteNotifications];
+    
+    
+    [Parse setApplicationId:@"YUQPxVyuVzkwpJhasBxz6gyM3fG2EWD3prK866Ci"
+                  clientKey:@"MSHXbe7k9tJLPIMPOUQsO81GIOPrh7W7M5joJvu3"];
+
     [GMSServices provideAPIKey:kGoogleAPIKey];
     
     //[[Crashlytics sharedInstance] crash];
@@ -34,6 +49,17 @@
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:deviceToken];
+    currentInstallation.channels = @[ @"global" ];
+    [currentInstallation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
