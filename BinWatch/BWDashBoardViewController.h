333e22b7506353b3d83c3a82f0f8114ae78fbd56
Supritha <supritha091@gmail.com>
2015-10-11 09:53:42 +0530

    Added Dashboard VC

---
+++ BinWatch/BWDashBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BWDashBoardViewController.h
+//  BinWatch
+//
+//  Created by Supritha Nagesha on 10/10/15.
+//  Copyright (c) 2015 Airwatch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWDashBoardViewController : UIViewController
+
+@end
---
+++ BinWatch/BWDashBoardViewController.m
@@ -0,0 +1,37 @@
+//
+//  BWDashBoardViewController.m
+//  BinWatch
+//
+//  Created by Supritha Nagesha on 10/10/15.
+//  Copyright (c) 2015 Airwatch. All rights reserved.
+//
+
+#import "BWDashBoardViewController.h"
+
+@interface BWDashBoardViewController ()
+
+@end
+
+@implementation BWDashBoardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
