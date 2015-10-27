786f921bfe6ab5270a2196cf96a1242d628cb8bc
Ponnie Rohith <ponnierohith+github@gmail.com>
2015-10-27 19:21:56 +0530

    mailer helper method to send mails with attachment

--- BinWatch/BinWatch/BWMailer.m
+++ BinWatch/BinWatch/BWMailer.m
@@ -47,6 +47,57 @@
     }
 }
 
++(void) composeMailWithSubject:(NSString*)subject body:(NSString*)body andAttachment:(NSString*)file
+{
+    UIViewController *controllerToShowTo = [[AppDelegate appDel] getTabBarContoller];
+    if ([MFMailComposeViewController canSendMail]) {
+        MFMailComposeViewController* controller = [[MFMailComposeViewController alloc] init];
+        controller.mailComposeDelegate = [self sharedInstance];
+        [controller setSubject:subject];
+        [controller setMessageBody:body isHTML:NO];
+        [controller setToRecipients:@[[[BWAppSettings sharedInstance] getSupportMailID]]];
+
+        // Determine the file name and extension
+        NSArray *filepart = [file componentsSeparatedByString:@"."];
+        NSString *filename = [filepart objectAtIndex:0];
+        NSString *extension = [filepart objectAtIndex:1];
+        
+        // Get the resource path and read the file using NSData
+        NSString *filePath = [[NSBundle mainBundle] pathForResource:filename ofType:extension];
+        NSData *fileData = [NSData dataWithContentsOfFile:filePath];
+        
+        // Determine the MIME type
+        NSString *mimeType;
+        if ([extension isEqualToString:@"jpg"]) {
+            mimeType = @"image/jpeg";
+        } else if ([extension isEqualToString:@"png"]) {
+            mimeType = @"image/png";
+        } else if ([extension isEqualToString:@"doc"]) {
+            mimeType = @"application/msword";
+        } else if ([extension isEqualToString:@"ppt"]) {
+            mimeType = @"application/vnd.ms-powerpoint";
+        } else if ([extension isEqualToString:@"html"]) {
+            mimeType = @"text/html";
+        } else if ([extension isEqualToString:@"pdf"]) {
+            mimeType = @"application/pdf";
+        }
+        
+        // Add attachment
+        [controller addAttachmentData:fileData mimeType:mimeType fileName:filename];
+        
+
+        if (controller)
+        {
+            [controllerToShowTo presentViewController:controller animated:YES completion:nil];
+        }
+    }
+    else
+    {
+        // Handle the error
+        [BWLogger DoLog:@"Mail not configured"];
+        [BWHelpers displayHud:@"Mail not configured" onView:controllerToShowTo.view];
+    }
+}
 - (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
 {
     UIViewController *controllerToShowTo = [[AppDelegate appDel] getTabBarContoller];
