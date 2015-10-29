4d2f5a5c14760ae84077fdb9b893eb380da42503
Supritha <supritha091@gmail.com>
2015-10-30 00:26:58 +0530

    no message

--- BinWatch/BinWatch/DataHandler/BWHelpers.h
+++ BinWatch/BinWatch/DataHandler/BWHelpers.h
@@ -30,7 +30,5 @@ void runOnMainThread(void(^block)(void));
 + (NSError *)generateError:(NSString *)errorMsg;
 + (NSString *)generateUniqueFilePath;
 + (NSString *)getPresentDateTimeString;
-+ (NSDate *)dateByAddingDays:(int)days toDate:(NSDate*)date;
-
 
 @end
